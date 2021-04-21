66e0848e2b7f6c0be1c11453cd8d4ed2f05cda35
Afonso-cmd <afonso.ur@gmail.com>
2021-04-21 19:45:14 -0300

    métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,21 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
+		method1();
+		
+		System.out.println("Fim do programa");
+		
+		
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -16,13 +31,15 @@ public class Program {
 		}
 		catch(ArrayIndexOutOfBoundsException e){// para posição maior que o vetor
 			System.out.println("Posição Inválida");
+			e.printStackTrace();//rastreamento do stack 
+			sc.hasNext();//para esperar eu digitar
 		}
 		catch(InputMismatchException e){// para posição maior que o vetor
 			System.out.println("Posição Errada");
 		}
-		System.out.println("Fim do programa");
 		
 		sc.close();
+		System.out.println("***METHOD2 END***");
 	}
 
 }
